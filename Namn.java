63273c728660635d826cd6141d6bf14790409014
Wille <te16.wille.pilquist@umea.nti.se>
2017-09-11 11:15:15 +0200

    FirstCommit

---
+++ Namn.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Namn {
+	public static void main (String[] arg) {
+		JOptionPane.showMessageDialog(null, "Click ok to get beaned!");
+		
+		JOptionPane.showMessageDialog(null, "Get prank'd!");
+	}
+}
